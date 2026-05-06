6a0d6c71ee658e35397ca9dcf131ee5d51833f70
karya-anak-bangsa <tkab.alamsyah@gmail.com>
2026-05-06 15:45:41 +0700

    Project ASEAN Hub

--- resources/views/pages/section-photo-gallery.blade.php
+++ resources/views/pages/section-photo-gallery.blade.php
@@ -4,5 +4,30 @@
         <div class="section-title">
             <h2>Photo Gallery and Moments</h2>
         </div>
+
+        <div class="moments-gallery" data-aos="fade-up" data-aos-delay="100">
+            <div class="row g-3">
+                <div class="col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="400">
+                    <a href="{{ asset('storage/photo-gallery/photo-gallery-stasiun-1.webp') }}" class="moment-item glightbox" data-gallery="campus-moments">
+                        <img src="{{ asset('storage/photo-gallery/photo-gallery-stasiun-1.webp') }}" class="img-fluid" loading="lazy" alt="Campus Moment">
+                        <div class="moment-overlay">
+                            <i class="bi bi-arrows-fullscreen"></i>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="400">
+                    <a href="{{ asset('storage/photo-gallery/photo-gallery-stasiun-2.webp') }}" class="moment-item glightbox" data-gallery="campus-moments">
+                        <img src="{{ asset('storage/photo-gallery/photo-gallery-stasiun-2.webp') }}" class="img-fluid" loading="lazy" alt="Campus Moment">
+                        <div class="moment-overlay">
+                            <i class="bi bi-arrows-fullscreen"></i>
+                        </div>
+                    </a>
+                </div>
+
+            </div>
+        </div><!-- End Moments Gallery -->
+
     </div>
 </section>
+
+{{-- : asset('storage/default/default-hero.webp'), --}}
