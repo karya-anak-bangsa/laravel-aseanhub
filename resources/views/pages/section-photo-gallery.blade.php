0fb5683827219ed8c45c07f63c588afff70e68a1
karya-anak-bangsa <tkab.alamsyah@gmail.com>
2026-05-07 10:35:14 +0700

    Project ASEAN Hub

--- resources/views/pages/section-photo-gallery.blade.php
+++ resources/views/pages/section-photo-gallery.blade.php
@@ -1,10 +1,63 @@
 <section id="photo-gallery" class="students-life section light-background">
+
+    <div class="container section-title">
+        <h2>Photo Gallery and Moments</h2>
+        <p>Documentation and urban moments from ASEAN HUB International Design Competition.</p>
+    </div>
+
     <div class="container">
+        <div class="moments-gallery">
+            <div class="row g-3">
+                <div class="col-md-6 col-lg-4">
+                    <a href="{{ asset('storage/photo-gallery/photo-gallery-stasiun-2.webp') }}" class="moment-item glightbox" data-gallery="campus-moments">
+                        <img src="{{ asset('storage/photo-gallery/photo-gallery-stasiun-2.webp') }}" class="img-fluid" loading="lazy" alt="Campus Moment">
+                        <div class="moment-overlay">
+                            <i class="bi bi-arrows-fullscreen"></i>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-6 col-lg-8">
+                    <a href="{{ asset('storage/photo-gallery/photo-gallery-stasiun-1.webp') }}" class="moment-item glightbox" data-gallery="campus-moments">
+                        <img src="{{ asset('storage/photo-gallery/photo-gallery-stasiun-1.webp') }}" class="img-fluid" loading="lazy" alt="Campus Moment">
+                        <div class="moment-overlay">
+                            <i class="bi bi-arrows-fullscreen"></i>
+                        </div>
+                    </a>
+                </div>
 
-        <div class="section-title">
-            <h2>Photo Gallery and Moments</h2>
-            <p>Documentation and urban moments from ASEAN HUB International Design Competition.</p>
+                <div class="col-md-6 col-lg-8">
+                    <a href="{{ asset('storage/photo-gallery/photo-gallery-stasiun-3.webp') }}" class="moment-item glightbox" data-gallery="campus-moments">
+                        <img src="{{ asset('storage/photo-gallery/photo-gallery-stasiun-3.webp') }}" class="img-fluid" loading="lazy" alt="Campus Moment">
+                        <div class="moment-overlay">
+                            <i class="bi bi-arrows-fullscreen"></i>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <a href="{{ asset('storage/photo-gallery/photo-gallery-stasiun-2.webp') }}" class="moment-item glightbox" data-gallery="campus-moments">
+                        <img src="{{ asset('storage/photo-gallery/photo-gallery-stasiun-2.webp') }}" class="img-fluid" loading="lazy" alt="Campus Moment">
+                        <div class="moment-overlay">
+                            <i class="bi bi-arrows-fullscreen"></i>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <a href="{{ asset('storage/photo-gallery/photo-gallery-stasiun-4.webp') }}" class="moment-item glightbox" data-gallery="campus-moments">
+                        <img src="{{ asset('storage/photo-gallery/photo-gallery-stasiun-4.webp') }}" class="img-fluid" loading="lazy" alt="Campus Moment">
+                        <div class="moment-overlay">
+                            <i class="bi bi-arrows-fullscreen"></i>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-6 col-lg-8">
+                    <a href="{{ asset('storage/photo-gallery/photo-gallery-stasiun-4.webp') }}" class="moment-item glightbox" data-gallery="campus-moments">
+                        <img src="{{ asset('storage/photo-gallery/photo-gallery-stasiun-4.webp') }}" class="img-fluid" loading="lazy" alt="Campus Moment">
+                        <div class="moment-overlay">
+                            <i class="bi bi-arrows-fullscreen"></i>
+                        </div>
+                    </a>
+                </div>
+            </div>
         </div>
-
     </div>
 </section>
