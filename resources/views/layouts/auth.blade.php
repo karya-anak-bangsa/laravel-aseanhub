def815bc5896c174abbd389a82d5f888fb9d9531
karya-anak-bangsa <tkab.alamsyah@gmail.com>
2026-05-30 10:34:57 +0700

    Project ASEAN Hub

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -36,7 +36,7 @@
             #auth-left {
                 background-image:
                     linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-                    url('{{ asset('/img/hero-monas-2.png') }}');
+                    url("{{ asset('/img/hero-monas-2.png') }}");
                 background-size: cover;
                 background-position: center;
                 background-repeat: no-repeat;
