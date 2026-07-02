4f0e500baafdbc3e9a335b4cc60f8ac7f83f5a9a
Karya Anak Bangsa <tkab.alamsyah@gmail.com>
2026-07-02 13:58:47 +0700

    -

--- database/seeders/JudgesSeeder.php
+++ database/seeders/JudgesSeeder.php
@@ -14,13 +14,85 @@ class JudgesSeeder extends Seeder
         // Judges::factory()->count(10)->assessment_one()->create();
         // Judges::factory()->count(10)->assessment_two()->create();
         // Judges::factory()->count(10)->final_assessment()->create();
+
+
         Judges::create([
-            'judges_name'           => 'Example Judges',
+            'judges_name'           => 'Her Pramtama',
             'origin_country'        => 'Indonesia',
-            'origin_institution'    => 'Institut Pertanian Bogor',
+            'origin_institution'    => null,
             'judges_task'           => 'Assessment One',
-            'judges_photo'          => 'judges/dummy/person-2.webp',
-            'email'                 => 'example.judges@gmail.com',
+            'judges_photo'          => 'judges/dummy/judges-her-pratama.webp',
+            'email'                 => 'example.judges-1@gmail.com',
+            'password'              => Hash::make('12341234'),
+        ]);
+
+        Judges::create([
+            'judges_name'           => 'Achmad D. Tardiyana',
+            'origin_country'        => 'Indonesia',
+            'origin_institution'    => null,
+            'judges_task'           => 'Assessment One',
+            'judges_photo'          => 'judges/dummy/judges-achmad-tardiyana.webp',
+            'email'                 => 'example.judges-2@gmail.com',
+            'password'              => Hash::make('12341234'),
+        ]);
+
+        Judges::create([
+            'judges_name'           => 'Marco Kusumawijaya',
+            'origin_country'        => 'Indonesia',
+            'origin_institution'    => null,
+            'judges_task'           => 'Assessment One',
+            'judges_photo'          => 'judges/dummy/judges-marco-kusumawijaya.webp',
+            'email'                 => 'example.judges-3@gmail.com',
+            'password'              => Hash::make('12341234'),
+        ]);
+
+        Judges::create([
+            'judges_name'           => 'Meyriana Kesuma',
+            'origin_country'        => 'Indonesia',
+            'origin_institution'    => null,
+            'judges_task'           => 'Assessment One',
+            'judges_photo'          => 'judges/dummy/judges-meyriana-kesuma.webp',
+            'email'                 => 'example.judges-4@gmail.com',
+            'password'              => Hash::make('12341234'),
+        ]);
+
+        Judges::create([
+            'judges_name'           => 'Prof. Bambang Susantono',
+            'origin_country'        => 'Indonesia',
+            'origin_institution'    => null,
+            'judges_task'           => 'Assessment One',
+            'judges_photo'          => 'judges/dummy/judges-bambang-susantono.webp',
+            'email'                 => 'example.judges-5@gmail.com',
+            'password'              => Hash::make('12341234'),
+        ]);
+
+        Judges::create([
+            'judges_name'           => 'Prof. Marlon Boarnet',
+            'origin_country'        => 'United States',
+            'origin_institution'    => null,
+            'judges_task'           => 'Assessment One',
+            'judges_photo'          => 'judges/dummy/judges-marlon.webp',
+            'email'                 => 'example.judges-6@gmail.com',
+            'password'              => Hash::make('12341234'),
+        ]);
+
+        Judges::create([
+            'judges_name'           => 'Prof Dr. Walter Timo de Vries',
+            'origin_country'        => 'Netherlands',
+            'origin_institution'    => null,
+            'judges_task'           => 'Assessment One',
+            'judges_photo'          => 'judges/dummy/judges-walter-1.webp',
+            'email'                 => 'example.judges-7@gmail.com',
+            'password'              => Hash::make('12341234'),
+        ]);
+
+        Judges::create([
+            'judges_name'           => 'Prof. Wim van den Doel',
+            'origin_country'        => 'Netherlands',
+            'origin_institution'    => null,
+            'judges_task'           => 'Assessment One',
+            'judges_photo'          => 'judges/dummy/judges-doel.webp',
+            'email'                 => 'example.judges-8@gmail.com',
             'password'              => Hash::make('12341234'),
         ]);
     }
--- resources/views/pages/section-judges.blade.php
+++ resources/views/pages/section-judges.blade.php
@@ -11,7 +11,7 @@
             {{-- Judges List --}}
             <div class="row g-4">
                 @foreach ($judges as $item)
-                    <div class="col-lg-3 col-md-6">
+                    <div class="col-lg-3 col-md-3">
                         <div class="staff-card">
                             <div class="card-image">
                                 <img
